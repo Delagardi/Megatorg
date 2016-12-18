4f66468720b37cb84086f3930e1ea4be0c04bb57
Delagardi <delagardi@redy.io>
2016-12-18 05:48:39 +0200

    index.html with out sliders

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(function(){
+    $('.view-source .hide').hide();
+    $a = $('.view-source a');
+    $a.on('click', function(event) {
+      event.preventDefault();
+      $a.not(this).next().slideUp(500);
+      $(this).next().slideToggle(500);
+    });
+  });
+});
